5844a1be07b3d8da326552a76a7bd82ebcb0e58a
pablo-tiernoparla <pablo.delgadosoto@tiernoparla.es>
2023-01-19 20:04:59 +0100

    llave maestra

--- src/main/java/daw/programacion/App.java
+++ src/main/java/daw/programacion/App.java
@@ -7,7 +7,7 @@ public class App
         Puerta puerta1 = new Puerta(1, false);
         Puerta puerta2 = new Puerta(2, false);
         PuertaBlindada puertab1 = new PuertaBlindada(6, false, 9, false);
-        Llave llave1 = new Llave(1, false);
+        Llave llave1 = new Llave(1, true);
         Llave llave2 = new Llave(2, false);
         Conserje conserje1 = new Conserje("Raul", new Llave[]{llave1, llave2});
 
